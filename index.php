901e0ac68c08d4ee73fea434e0c3cc902f4e9e38
AngeloNix64 <angelofernandez15@outlook.fr>
2024-03-28 12:38:15 +0100

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,50 @@
 
     <p>Exemple</p>
 
+    <section id="section1">
+            <!-- CADRE LEGALE -->
+            <div class="container"> <!-- container si je touche pas les bords pas de container si je touche les bords)-->
+                <div class="row"> <!-- ça aligne -->
+                    <div class="col-lg-6"> <!-- On copie ça le nombre de fois ou on a besoin -->
+                        <div id="" class="m-3">
+                            <h2>La Plateforme CRM <br>Que Toute Votre <br>Entreprise Adorera</h2>                
+                            <div class="col-lg-9 "> <!-- On copie ça le nombre de fois ou on a besoin -->
+                                <div class="mt-3">
+                                    Fournir un excellent service client. Répondez à plus de tickets tout-en-un Principalement un logiciel.
+                            </div> 
+                        </div>
+                    </div>
+                </div>
+
+            </div>  
+            <div class="row">              
+                <div class="col-lg-6"> <!-- On copie ça le nombre de fois ou on a besoin -->
+                    <div class="m-3">
+                        <button type="button" href="#" class="btn btn-dark">Commencer</button> <button type="button" href="#" class="btn">Comment Ça Fonctionne</button>
+                    <div class="m-3">
+                </div>
+            </div> 
+            </div> 
+                <div class="row"> <!-- ça aligne -->
+                    <div class="col-lg-3"> <!-- On copie ça le nombre de fois ou on a besoin -->
+                        <div id="" class="m-3">
+                            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptas laborum et quis consectetur tempore soluta, eius quasi. Vero ea maxime autem, in aliquam nemo possimus praesentium placeat harum maiores sit.
+                        </div>
+                    </div>
+                    <div class="col-lg-3"> <!-- On copie ça le nombre de fois ou on a besoin -->
+                        <div class="m-3">
+                            <h3>5 Stars Awards</h3>
+                        </div>
+                        <div class="m-3">
+                            For Customer Services
+                        </div>
+                    </div>
+                </div>
+        </section>
+
 </main>
+
+
 
 
 </body> </html>
