5e64051e047b3440928ab2089382dbc0e106485c
RayanDev23 <rayanquessada.rq@gmail.com>
2024-03-28 16:27:47 +0100

    login

---
+++ login.php
@@ -0,0 +1,42 @@
+<?php include "app/app.php";
+$page = 2;
+head($page); ?>
+
+<body>
+    <header> <?php navbar($page); ?>
+    </header>
+
+
+    <main>
+        <div class="container">
+            <div class="row">
+                <div class="col-12 text-center">
+                    <h1 class="">Se connecter</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos beatae a perspiciatis omnis repudiandae ratione unde, magnam praesentium atque fugiat ad, facilis consequuntur! Dicta animi officia voluptates, provident exercitationem reprehenderit!</p>
+                </div>
+            </div>
+            <div class="purple-div">
+                <div class="white-div">
+                    <h2>Se connecter</h2>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Incidunt modi hic voluptas dolore harum aliquid? Ipsum, eaque. Facere eum voluptate sint!evfvtgrtgtgrtgrtgrtg</p>
+            <form>
+                <div class="form-group mb-5">
+                    <input type="text" class="form-control" placeholder="Nom d'utilisateur">
+                </div>
+                <div class="form-group mb-5">
+                    <input type="password" class="form-control" placeholder="Mot de passe">
+                </div>
+                <div class="form-group mb-5">
+                    <input type="email" class="form-control" placeholder="Email">
+                </div>
+                <button type="submit" class="btn btn-dark">Envoyer</button>
+            </form>
+                </div>
+            </div>
+
+
+
+
+
+</html>
+</body>
