55c5603fb83cf32b8b25aebe94351c9da7375d56
AngeloNix64 <angelofernandez15@outlook.fr>
2024-03-29 11:43:51 +0100

    Update index.php

--- index.php
+++ index.php
@@ -2,53 +2,107 @@
 <body> <header> <?php navbar($page); ?> </header>
 
 <main>
-
-    <p>Exemple</p>
-
     <section id="section1">
-            <!-- CADRE LEGALE -->
-            <div class="container"> <!-- container si je touche pas les bords pas de container si je touche les bords)-->
-                <div class="row"> <!-- ça aligne -->
-                    <div class="col-lg-6"> <!-- On copie ça le nombre de fois ou on a besoin -->
-                        <div id="" class="m-3">
-                            <h2>La Plateforme CRM <br>Que Toute Votre <br>Entreprise Adorera</h2>                
-                            <div class="col-lg-9 "> <!-- On copie ça le nombre de fois ou on a besoin -->
-                                <div class="mt-3">
-                                    Fournir un excellent service client. Répondez à plus de tickets tout-en-un Principalement un logiciel.
-                            </div> 
-                        </div>
+        <!-- CADRE LEGALE -->
+        <div class="container mt-5 mb-5"> <!-- container si je touche pas les bords pas de container si je touche les bords)-->
+            <div class="row"> <!-- ça aligne -->
+                <div class="col-lg-6 mb-5"> <!-- On copie ça le nombre de fois ou on a besoin -->
+                    <div id="" class="m-3">
+                        <h2 class="lh-base">La Plateforme CRM <br>Que Toute Votre <br>Entreprise Adorera</h2>                
+                        <div class="col-lg-9 "> <!-- On copie ça le nombre de fois ou on a besoin -->
+                            <div class="mt-3">
+                                Fournir un excellent service client. Répondez à plus de tickets tout-en-un Principalement un logiciel.
+                            </div>
+                        </div> 
+                    </div>                
+                    <div class="col-lg-9"> <!-- On copie ça le nombre de fois ou on a besoin -->
+                        <div class="mt-3">
+                            <button type="button" href="#" class="btn btn-dark pe-5 ps-5 pt-2 pb-2">Commencer</button> 
+                            <button type="button" href="#" class="btn pe-2 ps-2 pt-2 pb-2">Comment Ça Fonctionne</button>
+                        <div class="mt-3">
                     </div>
                 </div>
-
-            </div>  
-            <div class="row">              
-                <div class="col-lg-6"> <!-- On copie ça le nombre de fois ou on a besoin -->
-                    <div class="m-3">
-                        <button type="button" href="#" class="btn btn-dark">Commencer</button> <button type="button" href="#" class="btn">Comment Ça Fonctionne</button>
-                    <div class="m-3">
-                </div>
-            </div> 
-            </div> 
-                <div class="row"> <!-- ça aligne -->
-                    <div class="col-lg-3"> <!-- On copie ça le nombre de fois ou on a besoin -->
-                        <div id="" class="m-3">
+            </div>
+        </div>  
+        <div class="row"> <!-- ça aligne -->
+            <div class="col-lg-3"> <!-- On copie ça le nombre de fois ou on a besoin -->
+                <div id="" class="m-3">
                             Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptas laborum et quis consectetur tempore soluta, eius quasi. Vero ea maxime autem, in aliquam nemo possimus praesentium placeat harum maiores sit.
-                        </div>
-                    </div>
-                    <div class="col-lg-3"> <!-- On copie ça le nombre de fois ou on a besoin -->
-                        <div class="m-3">
-                            <h3>5 Stars Awards</h3>
-                        </div>
-                        <div class="m-3">
-                            For Customer Services
+                </div>
+            </div>
+            <div class="col-lg-3"> <!-- On copie ça le nombre de fois ou on a besoin -->
+                <div class="m-3">
+                    <h3>5 Stars Awards</h3>
+                </div>
+                <div class="m-3">
+                    For Customer Services
+                </div>
+            </div>
+        </div>
+    </section>
+	<section id="sectionFP">
+        <!-- FONCTIONS PREMIUM -->
+        <div class="container">
+            <h2 class="text-center mt-3 mb-3">Explorez Les Fonctionnalités Premium</h2>
+			<p class="text-center mt-3 mb-3">Suite logicielle unique et puissante pour gérer l'ensemble de votre entreprise, présentée <br>par une entreprise ayant une vision à long terme pour transformer votre façon de travailler.</p>
+            <div class="row justify-content-center">
+            <!-- Card 1 -->
+                <div class="col-lg-4 mb-4">
+                    <div class="card p-4 shadow-sm border-light h-100" style="background-color: rgba(244, 244, 244, 0.93);">
+                        <div class="card-body">
+                            <svg class="mt-1 mb-3" width="64" height="64" viewBox="0 0 64 64" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <circle cx="41.8846" cy="42.3843" r="21.5" fill="#B1E8F2"/>
+                            <path d="M27.3846 40.2593C34.2192 40.2593 39.7596 34.7188 39.7596 27.8843C39.7596 21.0498 34.2192 15.5093 27.3846 15.5093C20.5501 15.5093 15.0096 21.0498 15.0096 27.8843C15.0096 34.7188 20.5501 40.2593 27.3846 40.2593Z" stroke="#393939" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/>
+                            <path d="M41.7445 11.668C42.3977 12.2695 43.0164 12.8883 43.6008 13.5242L50.6391 14.5297C51.7859 16.5213 52.6698 18.6531 53.2687 20.8719L48.9891 26.5695C48.9891 26.5695 49.0664 28.3227 48.9891 29.1992L53.2687 34.8969C52.6726 37.1166 51.7885 39.2487 50.6391 41.2391L43.6008 42.2445C43.6008 42.2445 42.3891 43.5078 41.7445 44.1008L40.7391 51.1391C38.7474 52.2859 36.6156 53.1698 34.3969 53.7687L28.6992 49.4891C27.8244 49.5664 26.9444 49.5664 26.0695 49.4891L20.3719 53.7687C18.1522 53.1726 16.02 52.2885 14.0297 51.1391L13.0242 44.1008C12.3883 43.4992 11.7695 42.8805 11.168 42.2445L4.12969 41.2391C2.98289 39.2474 2.09897 37.1156 1.5 34.8969L5.77969 29.1992C5.77969 29.1992 5.70234 27.4461 5.77969 26.5695L1.5 20.8719C2.0962 18.6522 2.98026 16.52 4.12969 14.5297L11.168 13.5242C11.7695 12.8883 12.3883 12.2695 13.0242 11.668L14.0297 4.62969C16.0213 3.48289 18.1531 2.59897 20.3719 2L26.0695 6.27969C26.9444 6.20233 27.8244 6.20233 28.6992 6.27969L34.3969 2C36.6166 2.5962 38.7487 3.48026 40.7391 4.62969L41.7445 11.668Z" stroke="#393939" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/>
+                            </svg>
+                            <h3 class="card-title">Paramètres puissants</h3>
+                            <p class="card-text">Fournir un excellent service client. Répondez à plus de tickets.</p>
                         </div>
                     </div>
                 </div>
-        </section>
-
+            <!-- Card 2 -->
+                <div class="col-lg-4 mb-4">
+                    <div class="card p-4 shadow-sm border-light h-100" style="background-color: #FFFFFF;">
+                        <div class="card-body">
+                            <svg class="mt-1 mb-3" width="70" height="61" viewBox="0 0 70 61" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <circle cx="48" cy="38.9421" r="21.5" fill="#FAD7C9"/>
+                            <path d="M25 35.4421C34.1127 35.4421 41.5 28.0548 41.5 18.9421C41.5 9.82944 34.1127 2.44214 25 2.44214C15.8873 2.44214 8.5 9.82944 8.5 18.9421C8.5 28.0548 15.8873 35.4421 25 35.4421Z" stroke="#303030" stroke-width="3" stroke-miterlimit="10"/>
+                            <path d="M1.5 46.4421C4.37931 42.9975 7.97529 40.2277 12.0352 38.3273C16.0951 36.4269 20.5204 35.4421 25 35.4421C29.4796 35.4421 33.9049 36.4269 37.9648 38.3273C42.0247 40.2277 45.6207 42.9975 48.5 46.4421" stroke="#303030" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/>
+                            <path d="M56 32.4421C58.4853 32.4421 60.5 30.6513 60.5 28.4421C60.5 26.233 58.4853 24.4421 56 24.4421C53.5147 24.4421 51.5 26.233 51.5 28.4421C51.5 30.6513 53.5147 32.4421 56 32.4421Z" stroke="#303030" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/>
+                            <path d="M55.5 24.4421V20.4421" stroke="#303030" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/>
+                            <path d="M52.5 26.4421L49.5 24.4421" stroke="#303030" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/>
+                            <path d="M52.5 30.4421L49.5 32.4421" stroke="#303030" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/>
+                            <path d="M55.5 32.4421V36.4421" stroke="#303030" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/>
+                            <path d="M59.5 30.4421L62.5 32.4421" stroke="#303030" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/>
+                            <path d="M59.5 26.4421L62.5 24.4421" stroke="#303030" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/>
+                            </svg>
+                            <h3 class="card-title">Facile à utiliser</h3>
+                            <p class="card-text">Utilisez la chronologie pour planifier les projets en fonction de la façon dont les pièces s'emboîtent.</p>
+                        </div>
+                    </div>
+                </div>
+                <!-- Card 3 -->
+                <div class="col-lg-4 mb-4">
+                    <div class="card p-4 shadow-sm border-light h-100" style="background-color: rgba(244, 244, 244, 0.93);">
+                        <div class="card-body">
+                            <svg class="mt-1 mb-3" width="77" height="77" viewBox="0 0 77 77" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <circle cx="45" cy="49.9421" r="21.5" fill="#F5C9E9"/>
+                            <path d="M11.0048 58.9373C8.43942 56.3719 10.1404 50.9902 8.82981 47.8393C7.51923 44.6883 2.5 41.9277 2.5 38.4421C2.5 34.9566 7.46346 32.3075 8.82981 29.045C10.1962 25.7825 8.43942 20.5123 11.0048 17.9469C13.5702 15.3816 18.9519 17.0825 22.1029 15.7719C25.2538 14.4614 28.0144 9.44214 31.5 9.44214C34.9856 9.44214 37.6346 14.4056 40.8971 15.7719C44.1596 17.1383 49.4298 15.3816 51.9952 17.9469C54.5606 20.5123 52.8596 25.8941 54.1702 29.045C55.4808 32.196 60.5 34.9566 60.5 38.4421C60.5 41.9277 55.5365 44.5768 54.1702 47.8393C52.8038 51.1018 54.5606 56.3719 51.9952 58.9373C49.4298 61.5027 44.0481 59.8018 40.8971 61.1123C37.7462 62.4229 34.9856 67.4421 31.5 67.4421C28.0144 67.4421 25.3654 62.4787 22.1029 61.1123C18.8404 59.746 13.5702 61.5027 11.0048 58.9373Z" stroke="#303030" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/>
+                            <path d="M43.5 31.4421L27.4909 47.4421L19.5 39.4421" stroke="#303030" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/>
+                            </svg>
+                            <h3 class="card-title">Plateforme intégrée</h3>
+                            <p class="card-text">Vivamus massa eleifend etiam neque odio feugiat dolor est.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 </main>
 
+<footer> <?php footer(); ?> </footer>
 
 
 
 </body> </html>
+
